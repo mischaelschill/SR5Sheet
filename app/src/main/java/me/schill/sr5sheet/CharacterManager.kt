48400b49b557605537926f87f8331ea0d07f0853
mischaelschill <mischael@schill.me>
2018-04-23 20:38:23 +0200

    Changed character storage directory and use uuid instead of character name as file name

--- app/src/main/java/me/schill/sr5sheet/CharacterManager.kt
+++ app/src/main/java/me/schill/sr5sheet/CharacterManager.kt
@@ -1,48 +1,55 @@
 package me.schill.sr5sheet
 
-import android.app.Application
-import android.content.Context
 import android.databinding.Observable
-import android.os.Environment
 import android.util.Log
 import me.schill.sr5sheet.model.SR5Character
 import me.schill.sr5sheet.model.Settings
-import java.io.File
 import org.simpleframework.xml.core.Persister
+import java.io.File
 import java.lang.Exception
+import java.util.*
 
 class CharacterManager: Observable.OnPropertyChangedCallback() {
     var settings = Settings()
     var current = SR5Character()
+    private var currentId = UUID::randomUUID.toString()
     private val serializer = Persister()
     private lateinit var filesDir:File
+    private lateinit var charDir:File
     private val settingsFile
         get() = filesDir.resolve("settings.xml")
-
+    private val loggerUnit = "CharacterManager"
 
     fun init(filesDir: File) {
         this.filesDir = filesDir
+        this.charDir = filesDir.resolve("char")
+        charDir.mkdirs()
 
         if (settingsFile.exists()) {
+            Log.i(loggerUnit, "Found settings file")
             try {
                 settings = serializer.read(Settings::class.java, settingsFile.inputStream())
             } catch (ex: Exception) {
+                Log.w(loggerUnit, "Unable to load settings file because of " + ex.message)
                 settings = Settings()
             }
         } else {
+            Log.i(loggerUnit, "Created new settings file")
             settings = Settings()
         }
 
-        val name =  settings.currentCharacter
+        val charId = settings.currentCharacter
 
-        if (name != null) {
-            current = loadImpl(name) ?: SR5Character()
-        } else {
-            current = SR5Character()
+        if (charId != null) {
+            Log.i(loggerUnit, "Trying to load previous character: " + charId)
+            loadImpl(charId)?.let {
+                current = it
+                currentId = charId
+                Log.i(loggerUnit, "Successfully loaded character")
+            }
         }
-        current.addOnPropertyChangedCallback(this);
-
-        settings.currentCharacter = current.name;
+        current.addOnPropertyChangedCallback(this)
+        settings.currentCharacter = currentId
         saveSettings()
     }
 
@@ -50,21 +57,22 @@ class CharacterManager: Observable.OnPropertyChangedCallback() {
         save()
     }
 
-    fun load(name: String): Boolean {
+    fun load(charId: String): Boolean {
         var result = false
-        loadImpl(name)?.let {
-            current.removeOnPropertyChangedCallback(this);
+        loadImpl(charId)?.let {
+            current.removeOnPropertyChangedCallback(this)
             current = it
-            current.addOnPropertyChangedCallback(this);
-            settings.currentCharacter = name;
+            current.addOnPropertyChangedCallback(this)
+            currentId = charId
+            settings.currentCharacter = charId
             saveSettings()
             result = true
         }
-        return result;
+        return result
     }
 
-    private fun loadImpl(name: String): SR5Character? {
-        val file = filesDir.resolve("$name.xml")
+    private fun loadImpl(charId: String): SR5Character? {
+        val file = charDir.resolve("$charId.xml")
         if (file.exists()) {
             try {
                 file.inputStream().use {
@@ -72,7 +80,7 @@ class CharacterManager: Observable.OnPropertyChangedCallback() {
                     return character
                 }
             } catch (ex: Exception) {
-                Log.e("CharacterManager", "Trying to load character", ex);
+                Log.e("CharacterManager", "Trying to load character", ex)
             }
         }
         return null
@@ -81,36 +89,22 @@ class CharacterManager: Observable.OnPropertyChangedCallback() {
     private fun saveSettings() {
         try {
             settingsFile.outputStream().use {
-                serializer.write(settings, it);
+                serializer.write(settings, it)
             }
         } catch (ex: Exception) {
-            Log.e("CharacterManager", "Trying to save settings", ex);
+            Log.e("CharacterManager", "Trying to save settings", ex)
         }
     }
 
     fun save(): Boolean {
-        val name = current.name
-        val file = filesDir.resolve("$name.xml")
+        val file = charDir.resolve("$currentId.xml")
         try {
             file.outputStream().use {
                 serializer.write(current, it)
             }
-            val oldName = settings.currentCharacter
-            if (oldName != name && oldName != null) {
-                try {
-                    val oldFile = filesDir.resolve("$oldName.xml");
-                    if (oldFile.exists()) {
-                        oldFile.delete();
-                    }
-                } catch (ex: Exception) {
-                    Log.e("CharacterManager", "Trying to delete old file", ex);
-                }
-                settings.currentCharacter = name;
-            }
-            saveSettings()
             return true
         } catch (ex: Exception) {
-            Log.e("CharacterManager", "Trying to save character", ex);
+            Log.e("CharacterManager", "Trying to save character", ex)
         }
         return false
     }
